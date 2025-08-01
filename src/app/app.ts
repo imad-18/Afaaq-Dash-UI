f4b47358e6657611fd5da654e119a9b80d0f694a
imad-18 <imad.labrini@uit.ac.ma>
2025-08-01 18:31:09 +0100

    add footer + update navbar

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,12 @@ import {Component, HostListener, signal} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {NzMenuDirective, NzMenuGroupComponent, NzMenuItemComponent, NzSubMenuComponent} from 'ng-zorro-antd/menu';
 import {Navbar} from './components/navbar/navbar';
+import {Footer} from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
