6feab32f08d89775470a1bbe3e988194a8d132d9
imad-18 <imad.labrini@uit.ac.ma>
2025-08-06 01:07:48 +0100

    remove footer + update ng.json to unable src/assets/ + get all campaigns

--- src/app/components/campaigns/campaigns.ts
+++ src/app/components/campaigns/campaigns.ts
@@ -1,19 +1,29 @@
-import {Component, inject, OnInit} from '@angular/core';
+import {Component, effect, inject, OnInit} from '@angular/core';
 import {NgForOf} from '@angular/common';
 import {CampaignService} from '../../services/campaign.service';
 import {Campaign} from '../../models/Campaign';
 import {NzTableComponent} from 'ng-zorro-antd/table';
+import {NzCarouselComponent, NzCarouselContentDirective} from 'ng-zorro-antd/carousel';
+import {NzCardComponent} from 'ng-zorro-antd/card';
+import {NzColDirective, NzRowDirective} from 'ng-zorro-antd/grid';
 
 @Component({
   selector: 'app-campaigns',
   imports: [
     NgForOf,
-    NzTableComponent
+    NzTableComponent,
+    NzCarouselComponent,
+    NzCarouselContentDirective,
+    NzCardComponent,
+    NzRowDirective,
+    NzColDirective
   ],
   templateUrl: './campaigns.html',
   styleUrl: './campaigns.css'
 })
 export class Campaigns implements OnInit {
+  array = [1, 2, 3, 4];
+  effect = 'scrollx';
 
   private campaignService = inject(CampaignService);
 
