daedc46f576811aeef69d8abcc636da8c85e279d
imad-18 <imad.labrini@uit.ac.ma>
2025-09-08 21:06:45 +0100

    update the statistic.ts & statistic.html to handle both campaigns & activities stats

--- src/app/models/Statistic.ts
+++ src/app/models/Statistic.ts
@@ -1,12 +1,16 @@
 import {StatisticAttribute} from './StatisticAttribute';
 import {Campaign} from './Campaign';
+import {Activity} from './Activity';
 
 export interface Statistic {
   id?: number;
   yearEdition: string;
   compaign?: Campaign;
+  activity?: Activity;
   attributes: StatisticAttribute[];
   compaignId: number;
   compaignTitle: string;
+  activityId: number;
+  activityTitle: string;
 
 }
